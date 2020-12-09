7f8a0a05de3b59f22415b95e93f6f30ef6f1b646
Quentin Danneville <quentin@qdan.io>
2020-12-09 15:42:50 +0100

    [Privacy] Add terms of use page

--- src/js/pages/settings/privacy.js
+++ src/js/pages/settings/privacy.js
@@ -1,10 +1,16 @@
 import React, { useState, useEffect } from 'react'
+import { NavLink } from 'react-router-dom'
 
 const UserPrivacy = (props) => {
 
     return <div className="w-full">
         <section className="common-container flex flex-col">
-            <h2>Privacy</h2>
+            <div className="flex w-full justify-between align-center my-6">
+                <h4 className="f3 text-white font-bold my-0">Privacy</h4>
+            </div>
+            <div>
+                <NavLink className="block underline f4 text-white" to="/terms-of-use">Terms of use</NavLink>
+            </div>
         </section>
     </div>
 }
